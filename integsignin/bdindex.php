62f72749e090e2787028264169cd4c428b438f1f
dronservice <140339705+dronservice@users.noreply.github.com>
2023-07-23 20:31:10 -0600

    Add files via upload

---
+++ integsignin/bdindex.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DataBases</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl4llNQApFmC26EwA0H8WgZl5MYYxFfc+NcPbldKGj7Sk" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcnd.com/bootstrap/4.5.0/js/booststrap.min.js" integrity="sha348-0gVRvuATPlz7JjHLku0U7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7B/
+    KR0JI" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div clas="container">
+        <h1>DataBases</h1>
+        <ul>
+            <li><a href="list.php">List Users</a></li>
+            <li><a href="add.php">Add Users</a></li>
+        </ul>
+    </div>
+</body>
+</html>
